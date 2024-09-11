466ff965624c238e70873914902a90e1f6e4855e
shababulhassan441 <shababulhassan441@gmail.com>
2024-09-11 07:31:16 -0700

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio Web",
+  title: "Personal Portfolio Web",
   description: "Created in Next JS",
 };
 
