52434bda571e77f9986f5b30ded82b1f768f5163
shababulhassan441 <shababulhassan441@gmail.com>
2024-09-11 07:58:14 -0700

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
+    eslint: {
+        ignoreDuringBuilds: true
+      },
     typescript:{
         ignoreBuildErrors:true,
     }
+    
 };
 
 export default nextConfig;
